247b93effe133d17df6deefce90479dd0078f4a3
Stefano Zanella <zanella.stefano@gmail.com>
2013-05-16 19:38:54 +0200

    Remove all current stuff + setup rspec-puppet.
    Rspec-puppet setup is not complete. It needs a <module_name> folder into
    spec/fixtures/modules. This is a task for the scaffold generator, since it
    relies of the name of the module, which can be decided only at module folder
    initialization.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'rspec-puppet'
 
-fixtures_path = File.expand_path('../../fixtures', __FILE__)
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-  c.module_path  = File.join(fixtures_path, 'modules')
-  c.manifest_dir = File.join(fixtures_path, 'manifests')
+  c.module_path  = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
