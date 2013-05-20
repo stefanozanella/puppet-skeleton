32c9a50e397a39d00e2cd3fe2f92bc165c5d4296
Stefano Zanella <zanella.stefano@gmail.com>
2013-05-20 16:02:52 +0200

    Remove all content that is not handled yet.
    I'm starting to wonder why did I clone the skeleton repo, if now I'm restarting
    almost from scratch. Whatever, I need to better define the boundary between
    Puppetry and the skeleton module.

--- Puppetfile
+++
@@ -1,5 +0,0 @@
-# vi: set ft=ruby :
-
-# Add module dependencies for this Puppet module here.
-
-#mod "apt", :git => "git://github.com/puppetlabs/puppetlabs-apt.git"
--- manifests/init.pp
+++
@@ -1,5 +0,0 @@
-# Install/configure something here
-class skeleton {
-  # Replace this with meaningful resources
-  exec { 'sample_command': command => '/bin/true' }
-}
